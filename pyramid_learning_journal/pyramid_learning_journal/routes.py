5e4396ef1ae3db6459fc931071a0ff19669ad69a
David Franklin <dave5801@gmail.com>
2017-10-31 18:40:39 -0700

    removed old files

--- pyramid_learning_journal/pyramid_learning_journal/routes.py
+++
@@ -1,9 +0,0 @@
-def includeme(config):
-    """Include the following routes for static files and uri paths."""
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('list_view', '/')
-    config.add_route('detail_view', '/detail_view')
-    config.add_route('create_view', '/create_view')
-    config.add_route('update_view', '/update_view')
-
-#learning_journal/journal/5
